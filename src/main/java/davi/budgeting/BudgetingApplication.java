ec2f84f1d9915f794a7666fce8c25b40c87e57e1
Davi Tavares <dxrktavares3@gmail.com>
2026-04-28 18:28:48 -0300

    feat: adiciona bean para chatclient

--- src/main/java/davi/budgeting/BudgetingApplication.java
+++ src/main/java/davi/budgeting/BudgetingApplication.java
@@ -1,10 +1,22 @@
 package davi.budgeting;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.beans.BeanProperty;
 
 @SpringBootApplication
 public class BudgetingApplication {
+
+    //Builder é injetado pela autoconfiguração do springboot
+    //Com isso, criamos o chatClient
+    @Bean
+    ChatClient chatChatClient(ChatClient.Builder builder) {
+        return builder.build();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetingApplication.class, args);
