9232fc79b0864f87e2cfe13968811e2365bb6dac
Davi Tavares <dxrktavares3@gmail.com>
2026-04-28 18:29:39 -0300

    feat: cria controller de chatmodel com função get

---
+++ src/main/java/davi/budgeting/ChatModelController.java
@@ -0,0 +1,23 @@
+package davi.budgeting;
+
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChatModelController {
+    private final OpenAiChatModel openAiChatModel;
+
+    public ChatModelController(OpenAiChatModel openAiChatModel){
+        this.openAiChatModel = openAiChatModel;
+    }
+
+    //Envia requisição
+    //Exemplo: "Olá!" e ao bater no endpoint, chatmodel retorna "Olá! Como posso ajudar?"
+    @GetMapping("/chat-model")
+    String chat(String prompt) {
+        return this.openAiChatModel.call(prompt);
+    }
+}
